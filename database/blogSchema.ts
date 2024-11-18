1ebca2096af65f54056dd0ab3c175d2959c16f85
vmagdangal <vmagdangal257@gmail.com>
2024-11-18 14:19:56 -0800

    feat: slug getter and mongoose schema

---
+++ database/blogSchema.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+// typescript type (can also be an interface)
+type Blog = {
+    title: string;
+    slug: string; 
+    date: Date;
+    description: string; // for preview
+    content: string; // for individual blog page
+    comments: Comment[]; // array for comments
+    image: string;
+    imageAlt: string;
+};
+
+
+// mongoose schema 
+const blogSchema = new Schema<Blog>({
+		title: { type: String, required: true },
+		slug: { type: String, required: true },
+		date: { type: Date, required: false, default: new Date()},
+		description: { type: String, required: true },
+		content: { type: String, required: true },
+        image: { type: String, required: true },
+        imageAlt: { type: String, required: false },
+})
+
+// defining the collection and model
+const Blog = mongoose.models['blogs'] ||
+    mongoose.model('blogs', blogSchema);
+
+export default Blog;
